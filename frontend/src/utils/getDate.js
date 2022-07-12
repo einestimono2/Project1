9ac9502b97717dfd38cb9d2586917a9dc033f56f
Tee <einestimono2@gmail.com>
2022-07-13 01:17:19 +0700

    fix date

--- frontend/src/utils/getDate.js
+++ frontend/src/utils/getDate.js
@@ -1,7 +1,7 @@
 exports.getBeginningOfTheDay = () => {
   const now = new Date();
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-  return today;
+  return today.toISOString();
 };
 
 exports.getBeginningOfTheWeek = () => {
@@ -9,17 +9,17 @@ exports.getBeginningOfTheWeek = () => {
   const days = (now.getDay() + 7 - 1) % 7;
   now.setDate(now.getDate() - days);
   now.setHours(0, 0, 0, 0);
-  return now;
+  return now.toISOString();
 };
 
 exports.getBeginningOfTheMonth = () => {
   var date = new Date();
   var firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
-  return firstDayOfMonth;
+  return firstDayOfMonth.toISOString();
 };
 
 exports.getBeginningOfTheYear = () => {
   var date = new Date();
   var firstDayOfYear = new Date(date.getFullYear(), 0, 1);
-  return firstDayOfYear;
+  return firstDayOfYear.toISOString();
 };
